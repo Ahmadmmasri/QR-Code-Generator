e40bda1bd44a2318bb6dcd5b6fdf68173973911e
Ahmadmmasri <71886488+Ahmadmmasri@users.noreply.github.com>
2020-12-11 15:17:07 +0200

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,6 +1,27 @@
 import os
-import qrcode 
+import qrcode
+import sys
+def __init__():
+    print('Welcome')
+    
+def start():
+    try:
+        link=input("enter link to generate qr code: ")
+        if not link=="":
+            img=qrcode.make(link)
+            img.save("qr.png", "PNG")
+            os.system("start qr.png")
+        else:
+            print('insert link')
+            start()
+    except:
+        print('someting wrong happend')
+start()
 
-img=qrcode.make("https://www.youtube.com/watch?v=kM4oZTJaO8k")
-img.save("qr.png", "PNG")
-os.system("start qr.png")
+print('Qr code generated seccesfully')
+inp=input('to generate another code press 1 to end press 2')
+if int(inp)==1:
+    start()
+else:
+    sys.exit()
+
